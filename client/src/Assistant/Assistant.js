5a7b276f50706a5af8cc477594246f19b11fd5d7
Vincent Ahrend <mail@vincentahrend.com>
2017-05-04 14:17:36 +0200

    Add flow type checking

--- client/src/Assistant/Assistant.js
+++ client/src/Assistant/Assistant.js
@@ -1,24 +1,25 @@
+// @flow
+
 import React from 'react';
 import autoBind from 'react-autobind';
 
 import BasicData from './BasicData';
 
 class Assistant extends React.Component {
-	constructor(props) {
+	state = {
+		stage: 0
+	}
+
+	constructor(props: {}) {
 		super(props);
-		this.state = {
-			stage: 0
-		}
 		autoBind(this);
 	}
 
 	render() {
 		switch(this.state.stage) {
 			case 0:
-				return <BasicData />;
-
 			default:
-				return "Nah";
+				return <BasicData />;
 		}
 	}
 }
