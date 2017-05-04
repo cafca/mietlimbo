d52898aa8f655678e9391510a5a63cc4031fc1ec
Vincent Ahrend <mail@vincentahrend.com>
2017-05-04 17:12:56 +0200

    I18n setup and introduction form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
+// @flow
+
 import React, { Component } from 'react';
+import { IntlProvider } from 'react-intl';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,7 +14,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Willkommen bei mietlimbo</h2>
         </div>
         <div className="App-intro">
           <Assistant />
@@ -20,4 +24,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const AppIntl = () => {
+  return <IntlProvider 
+    locale={"en"}
+  >
+    <App />
+  </IntlProvider>;
+}
+
+export default AppIntl;
--- client/src/Assistant/Assistant.js
+++ client/src/Assistant/Assistant.js
@@ -4,10 +4,28 @@ import React from 'react';
 import autoBind from 'react-autobind';
 
 import BasicData from './BasicData';
+import Introduction from './Introduction';
+import { FormattedMessage, FormattedDate } from 'react-intl';
+
+const Header = (props) => {
+  const style = {
+    main: {
+      marginBottom: "2em"
+    },
+    items: {
+      marginRight: 5
+    }
+  }
+  return <section style={style.main}>
+    <span style={style.items}>#{props.serialNumber}</span>
+    <span style={style.items}><FormattedDate value={new Date()} /></span>
+  </section>;
+}
 
 class Assistant extends React.Component {
 	state = {
-		stage: 0
+		stage: 0,
+		serialNumber: "03"
 	}
 
 	constructor(props: {}) {
@@ -15,12 +33,32 @@ class Assistant extends React.Component {
 		autoBind(this);
 	}
 
+	handleContinue(e: Event) {
+		this.setState({stage: (this.state.stage + 1)});
+	}
+
 	render() {
+		let content;
 		switch(this.state.stage) {
+			case 1:
+				content = <BasicData />;
+				break;
+
 			case 0:
 			default:
-				return <BasicData />;
+				content = <Introduction serialNumber={this.state.serialNumber} />;
 		}
+
+		return <div className="assistant">
+          <Header serialNumber={this.state.serialNumber} stage={this.state.stage} />
+          {content}
+          <button onClick={this.handleContinue}>
+          	<FormattedMessage
+          		id="Assistant.continue"
+          		defaultMessage="Continue"
+          	/>
+          </button>
+		</div>;
 	}
 }
 
