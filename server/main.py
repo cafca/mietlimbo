75b9fdffc9ca542b9bc802d639fc29129666267c
Vincent Ahrend <mail@vincentahrend.com>
2017-06-01 16:32:46 +0200

    Flask boilerplate

--- server/main.py
+++ server/main.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 """
-Module Docstring
+Documentation
+
+See also https://www.python-boilerplate.com/flask
 """
+import os
 
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS
 from logger import setup_logger
 
 logger = setup_logger(logfile=None)
 
 
-def main():
-    """ Main entry point of the app """
-    logger.info("hello world")
+def create_app(config=None):
+    app = Flask(__name__)
+
+    # See http://flask.pocoo.org/docs/0.12/config/
+    app.config.update(dict(DEBUG=True, SECRET_KEY="development key"))
+    app.config.update(config or {})
+
+    # Setup cors headers to allow all domains
+    # https://flask-cors.readthedocs.io/en/latest/
+    CORS(app)
+
+    # Definition of the routes. It's probably a good idea to break them out
+    # into their own file soon. See also Flask Blueprints:
+    # http://flask.pocoo.org/docs/0.12/blueprints
+    @app.route("/")
+    def hello_world():
+        logger.info("/")
+        return "Hello World"
+
+    @app.route("/foo/<someId>")
+    def foo_url_arg(someId):
+        logger.info("/foo/%s", someId)
+        return "Test: " + someId
+
+    return app
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+    port = int(os.environ.get("PORT", 8000))
+    app = create_app()
+    app.run(host="0.0.0.0", port=port)
