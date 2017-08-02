fc2bb957bb1318846e863e93d8d3b9d4dbc60644
Vincent Ahrend <mail@vincentahrend.com>
2017-08-03 00:14:23 +0200

    Parse and store streets

---
+++ api/crawler.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+import logging
+from logger import setup_logger
+from parser import MietspiegelParser
+from datetime import datetime, timedelta
+from time import sleep
+
+logger = setup_logger(logfile="./crawler.log", level=logging.INFO)
+
+LETTERS = "abcdefghiklmnopqrstuvwxyzäöüß-"
+STARTING_QUERY = "aaaa"
+QLEN = 4
+STREET_NAMES = "../streetnames.csv"
+JSON_STREETS = "../streets.json"
+STARTAT = 96
+
+class Limiter(object):
+	# limit in calls per second
+	limit = timedelta(seconds=2)
+	last = datetime.now()
+
+	def wait(self):
+		now = datetime.now()
+		dt = now - self.last
+		if dt < self.limit:
+			sleep_time = (self.limit - dt).seconds + float(0.000001) * (self.limit - dt).microseconds
+			sleep(sleep_time)
+		self.last = datetime.now()
+		return dt
+
+
+def next_query(q):
+	# Produces next permutation of q
+	for pos in range(QLEN):
+		nxt = (LETTERS.find(q[pos]) + 1) % len(LETTERS)
+		q = q[:pos] + LETTERS[nxt] + q[pos + 1:]
+		if nxt != LETTERS[0]:
+			break
+	return q
+
+def load_street_names():
+	# Convert csv street names into json
+	streets = []
+	i = 0
+	with open(STREET_NAMES) as f:
+		with open("../streets.json", "w") as f1:
+			reader = csv.reader(f, delimiter=";")
+			for row in reader:
+				streets.append(row[1])
+				i += 1
+				if i % 100 == 0:
+					logger.info("{}: {}".format(i, row[1]))
+			logger.info("{}: {}".format(i, row[1]))
+			json.dump(streets, f1)
+
+def main():
+	logger.info("Starting crawling")
+	QUERIES = set(["STRNAME"])
+	ps = MietspiegelParser()
+	limiter = Limiter()
+	with open(JSON_STREETS) as f:
+		streets = json.load(f)
+
+	last = ""
+	i = 0
+	numstreets = len(streets)
+	for cur in streets:
+		q = cur[:4]
+		i += 1
+		if i < STARTAT:
+			continue
+		elif q != last and q not in QUERIES:
+			ps.find_street(q)
+			QUERIES.add(q)
+
+			last = q
+			dt = limiter.wait()
+			logger.info("Finished {}/{} ({} %)".format(i, numstreets, round((100.0 * i) / numstreets, 2)))
+
+
+
+if __name__ == "__main__":
+	from main import create_app
+	app = create_app()
+	app.app_context().push()
+	with app.app_context():
+		main()
