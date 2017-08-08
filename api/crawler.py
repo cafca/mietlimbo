14912ec998dc3db2755ea36f0ab69aa373bbabb8
Vincent Ahrend <mail@vincentahrend.com>
2017-08-08 16:43:48 +0200

    Crawler documentation

--- api/crawler.py
+++ api/crawler.py
@@ -17,11 +17,14 @@ STREET_NAMES = "../streetnames.csv"
 JSON_STREETS = "../streets.json"
 
 class Limiter(object):
-	# limit in calls per second
+	# Minimum time that should have passed between calls to self.wait
 	limit = timedelta(seconds=1)
+
+	# Time of last call
 	last = datetime.now()
 
 	def wait(self):
+		"""Blocks until self.limit has passed since last call."""
 		now = datetime.now()
 		dt = now - self.last
 		if dt < self.limit:
@@ -41,13 +44,18 @@ def next_query(q):
 	return q
 
 def load_street_names():
-	# Convert csv street names into json
+	"""Convert CSV street names to JSON encoded list.
+
+	CSV is loaded from STREET_NAMES and JSON is written
+	to JSON_STREETS.
+	"""
 	streets = []
 	i = 0
 	with open(STREET_NAMES) as f:
-		with open("../streets.json", "w") as f1:
+		with open(JSON_STREETS, "w") as f1:
 			reader = csv.reader(f, delimiter=";")
 			for row in reader:
+				# Only append street name, discarding geo pos
 				streets.append(row[1])
 				i += 1
 				if i % 100 == 0:
@@ -56,9 +64,19 @@ def load_street_names():
 			json.dump(streets, f1)
 
 def crawl_street_names():
+	"""Crawl address names and IDs from Mietspiegel website.
+
+	A list of candidate names is read from JSON_STREETS and
+	queries are sent to the address database, embedded in the
+	Mietspiegel website. Queries consist of the leading four
+	characters of each candidate, with duplicate 4-character
+	queries avoided. A limiter is used to restrict call volume.
+
+	"""
 	STARTAT = 8985
 
 	logger.info("Starting crawling")
+	# 'STRNAME' is the CSV file's header for the street col
 	QUERIES = set(["STRNAME"])
 	ps = MietspiegelParser()
 	limiter = Limiter()
@@ -84,6 +102,11 @@ def crawl_street_names():
 
 
 def crawl_street_data():
+	"""Crawl Mietspiegel data for a list of street IDs
+
+	
+	
+	"""
 	from model import Street
 	ps = MietspiegelParser()
 	cookies = ps.get_cookies()
@@ -96,11 +119,13 @@ def crawl_street_data():
 
 	numstreets = len(streets)
 	for street in streets[i:]:
+		# From time to time, get new cookies
 		if i%25 == 0: cookies = ps.get_cookies()
 		i += 1
 		try:
 			ps.get_range(street.id, None, cookies=cookies)
 		except Exception as e:
+			# Live debugging :o
 			import pdb
 			pdb.set_trace()
 		dt = limiter.wait()
@@ -113,4 +138,8 @@ if __name__ == "__main__":
 	app = create_app()
 	app.app_context().push()
 	with app.app_context():
+		# First step
+		crawl_street_names()
+
+		# Second step
 		crawl_street_data()
