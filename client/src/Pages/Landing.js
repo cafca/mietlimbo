193e9d41464e0a1648df4962142c89ac2b145bd9
Vincent Ahrend <mail@vincentahrend.com>
2017-08-14 18:34:03 +0200

    Fix stage changing

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Landing = () => <div>
   <p>Huhu</p>
