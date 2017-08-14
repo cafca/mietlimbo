6ccadfec8b85303b67b583e73d3019ead5b8c758
Vincent Ahrend <mail@vincentahrend.com>
2017-08-14 18:55:42 +0200

    Navigation + about page

---
+++ client/src/Pages/About.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Landing = () => <div>
+  <p>About</p>
+  <p><Link to="/app/">Los</Link></p>
+</div>;
+
+export default Landing;
