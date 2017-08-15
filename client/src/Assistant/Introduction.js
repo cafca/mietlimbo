aaf40f956d4d7217255c8718833d85fe14debd13
Vincent Ahrend <mail@vincentahrend.com>
2017-08-15 16:23:22 +0200

    Styles and fonts

--- client/src/Assistant/Introduction.js
+++ client/src/Assistant/Introduction.js
@@ -1,36 +1,44 @@
 // @flow
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import {red500} from 'material-ui/styles/colors';
+
+const styles = {
+  bigtext: {
+    fontSize: 28
+  },
+  warning: {
+    color: red500
+  },
+  text: {
+    fontSize: 18
+  }
+};
 
 const Introduction = (props: {serialNumber: string}) => {
-  return <section>
-    <p><FormattedMessage
-      id="Introduction.welcome"
-      defaultMessage={`Hey Mieter!`}
-      values={props}
-    /></p>
-    <p><FormattedMessage
+  return <section style={styles.wrapper} >
+    <p style={styles.bigtext}><FormattedMessage
       id="Introduction.overview"
-      defaultMessage={`Hier findest du in den nächsten 30 Minuten heraus, 
+      defaultMessage={`Hey Mieter! Hier findest du in den nächsten 30 Minuten heraus, 
         wieviel Geld du mit der Mietpreisbremse sparen kannst und was genau 
-        der erste Schritt ist, um damit zu beginnen. Mit etwas Mut und Geduld kannst du nicht nur einen Haufen Geld 
-        sparen, sondern auch deinen Kiez davor schützen, bei explosiven 
+        der erste Schritt ist, um damit zu beginnen. Mit etwas Mut und Geduld könntest du einen Haufen Geld 
+        sparen und zugleich deinen Kiez davor schützen, bei explosiven 
         Mietsteigerungen sein Gesicht zu verlieren.`}
     /></p>
-    <p style={{color: "red"}}>
+    <p style={styles.warning}>
       <FormattedMessage
         id="Introduction.warning"
         defaultMessage="Achtung! Mietlimbo wird gerade noch gestestet und ist definitiv noch nicht fehlerfrei. 
           Die Informationen auf dieser Seite solltest du nicht direkt für bare Münze nehmen." />
     </p>
-    <p>
+    <p style={styles.text}>
       <FormattedMessage
         id="Introduction.requirements"
-        defaultMessage={`Um möglichst schnell und einfach zum Ergebnis zu kommen,
+        defaultMessage={`Um möglichst schnell und einfach zum Ergebnis zu kommen
           helfen einige Dinge enorm:`}
       />
     </p>
-    <ul className="inline-list">
+    <ul className="inline-list" style={styles.text}>
       <li>
         <FormattedMessage
           id="Introduction.requirement1"
@@ -46,18 +54,18 @@ const Introduction = (props: {serialNumber: string}) => {
       <li>
         <FormattedMessage
           id="Introduction.requirement2"
-          defaultMessage={`Mietzins des Vormieters`}
+          defaultMessage={`Kaltmiete des Vormieters`}
         />
       </li>
       <li>
         <FormattedMessage
           id="Introduction.requirement2"
-          defaultMessage={`Der Wohnung selbst! Bei manchen Fragen muss man
+          defaultMessage={`Die Wohnung selbst! Bei manchen Fragen muss man
             einfach mal eben nachgucken.`}
         />
       </li>
     </ul>
-    <p>
+    <p style={styles.text}>
       <FormattedMessage
         id="Introduction.requirementsUnavailable"
         defaultMessage={`Wenn nicht alles zur Hand ist, dann ist das auch nicht schlimm, du kannst auch 
@@ -65,7 +73,7 @@ const Introduction = (props: {serialNumber: string}) => {
           anderer Meinung ist, kannst du immernoch genauer nachforschen.`}
         />
     </p>
-    <p><FormattedMessage
+    <p style={styles.text}><FormattedMessage
       id="Introduction.nextSteps"
       defaultMessage={`Am Ende dieses Assistenten hast du die wichtigsten 
         Informationen für eine Mietpreisbremse zusammen. Hiermit kannst du ein
@@ -73,14 +81,14 @@ const Introduction = (props: {serialNumber: string}) => {
         Mietrüge'), mit dem du ihn aufforderst, deine Miete zu senken. 
 `}
     /></p>
-    <p><FormattedMessage
+    <p style={styles.text}><FormattedMessage
       id="Introduction.legalNote"
       defaultMessage={`Wenn du 
         noch unsicher bist und eine Rechtsberatung zu deiner individuellen 
         Situation möchtest, kannst du mit dem Ergebnis auch einfach zu einem 
         Mieterbund gehen, wo dir beim weiteren Vorgehen geholfen wird. Beachte 
         bitte, dass Mietlimbo keine Rechtsberatung ist und eine Rechtsberatung 
-        auch nicht ersetzen kann. Ich stelle dir hier nach meinen Möglichkeiten 
+        auch nicht ersetzen kann. Ich stelle dir hier kostenfrei nach meinen Möglichkeiten 
         korrekte und vollständige Informationen über die Mietpreisbremse zur Verfügung. 
         Ich übernehme allerdings keine Gewähr für die Richtigkeit, Vollständigkeit 
         und Aktualität der Informationen. 
