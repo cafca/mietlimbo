d396c9d193e6f0ae5454631fec33ddf443151842
Vincent Ahrend <mail@vincentahrend.com>
2017-08-28 17:13:36 +0200

    Add locale switcher

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // @flow
 
 import React, { Component } from 'react';
+import autoBind from "react-autobind";
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { IntlProvider, addLocaleData } from 'react-intl';
 import de from 'react-intl/locale-data/de';
 import en from 'react-intl/locale-data/en';
+import messagesEN from './I18n/en.json';
 
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -16,32 +20,40 @@ import About from './Pages/About';
 import Theme from './Theme.js'
 import './App.css'; 
 
-import messagesEN from './I18n/en.json';
-
-const locale = "de";
-
-addLocaleData([...de, ...en]);
-
-const messages = {
+const translations = {
   en: messagesEN
 };
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <div className="App-main">
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/app/" component={Assistant} />
-            <Route path="/app/:stage/" component={Assistant} />
-            <Route path="/about/" component={About} />
-          </div>
-        </div>  
-      </BrowserRouter>
-    );
-  }
-}
+addLocaleData([...de, ...en]);
+
+
+type AppProps = {
+  locale: string,
+  changeLocale: string => any
+};
+
+const App = (props: AppProps) => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <DropDownMenu 
+          style={{float: "right", marginRight: -10}} 
+          underlineStyle={{display: "none"}}
+          value={props.locale} 
+          onChange={(e,k,locale) => props.changeLocale(locale)}>
+          <MenuItem label="🇩🇪" primaryText="Deutsch" value="de" />
+          <MenuItem label="🇬🇧" primaryText="English" value="en" />
+        </DropDownMenu>
+        <div className="App-main">
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/app/" component={Assistant} />
+          <Route path="/app/:stage/" component={Assistant} />
+          <Route path="/about/" component={About} />
+        </div>
+      </div>  
+    </BrowserRouter>
+  );
+};
 
 const formats = {
   number: {
@@ -57,22 +69,38 @@ const formats = {
   }
 };
 
-const AppMaterialUI = () => (
+const AppMaterialUI = (props: AppProps) => (
   <MuiThemeProvider muiTheme={getMuiTheme(Theme)}>
-    <App />
+    <App changeLocale={props.changeLocale} locale={props.locale} />
   </MuiThemeProvider>
 );
 
-const AppIntl = () => {
-  return <IntlProvider 
-    formats={formats}
-    defaultFormats={formats}
-    locale={locale}
-    messages={messages[locale]}
-    defaultLocale={"de"}
-  >
-    <AppMaterialUI />
-  </IntlProvider>;
+class AppIntl extends Component {
+  state : {
+    locale: string
+  };
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      locale: "de"
+    };
+    autoBind(this);
+  }
+
+  render() {
+    return <IntlProvider 
+      formats={formats}
+      defaultFormats={formats}
+      locale={this.state.locale}
+      messages={translations[this.state.locale]}
+      defaultLocale={"de"}
+    >
+      <AppMaterialUI 
+        changeLocale={locale => this.setState({locale})}
+        locale={this.state.locale} />
+    </IntlProvider>;
+  }
 }
 
 export default AppIntl;
