34156316ba354a4583f7e5b676491ba065560a4e
Vincent Ahrend <mail@vincentahrend.com>
2017-08-29 23:33:18 +0200

    OfficialNames must always be German

--- client/src/Assistant/ApartmentFeatureInputs/FeatureOfficialNames.js
+++ client/src/Assistant/ApartmentFeatureInputs/FeatureOfficialNames.js
@@ -1,333 +1,87 @@
 // @flow
 
-import { defineMessages } from 'react-intl';
+export default {
+  WindowStyleSingle: "Überwiegend Einfachverglasung",
+  WindowStyleInsulating: "Überwiegend Wärmeschutzverglasung (Einbau ab 1995) oder Schallschutzfenster für Wohngebäude/Wohnungen, die vor 1995 bezugsfertig geworden sind",
+  HighGradeFlooring: "Hochwertiges Parkett, Natur-/Kunststein, Fliesen oder gleichwertiger Boden/-belag in der überwiegenden Zahl der Wohnräume",
+  InsufficientPower: "Unzureichende Elektroinstallation, z.B. keine ausreichende Elektrosteigleitung und/oder VDE gerechte Elektroinstallation (z.B. kein FI-Schalter, Potentialausgleich)",
+  WallInstallationElectrical: "Elektroinstallation überwiegend sichtbar auf Putz",
+  WallInstallationDrainage: "Be- und Entwässerungsinstallation überwiegend auf Putz",
+  WallInstallationHeating: "Heizungsrohre überwiegend nicht sichtbar",
+  ColdWaterMetering: "Wohnungsbezogener Kaltwasserzähler in vor 1998 bezugsfertigen Gebäuden/Wohnungen, wenn der Mieter nicht die Kosten für Miete oder Leasing im Rahmen der Betriebskosten trägt",
+  BadFlatDesign: "Schlechter Schnitt (z.B. gefangener Raum und/oder Durchgangsraum)",
+  StorageCabinet: "Einbauschrank oder Abstellraum innerhalb der Wohnung",
+  NoSpaceForWashing: "Waschmaschine weder in Bad noch Küche stellbar oder nicht anschließbar",
+  LargeLiving: "Großer, geräumiger Balkon, (Dach-)Terrasse, Loggia oder Winter-/Dachgarten (ab 4 m²)",
+  AccessibleDesign: "Barrierearme Wohnungsgestaltung (Schwellenfreiheit in der Wohnung, schwellenarmer Übergang zu Balkon/Terrasse, ausreichende Bewegungsfreiheit in der Wohnung und/oder barrierearme Badgestaltung)",
+  NoBalcony: "Kein Balkon, (Dach-)Terrasse, Loggia und Winter-/Dachgarten (gilt nicht, wenn das Merkmal aus baulichen und/oder rechtlichen Gründen nicht möglich oder nicht zulässig ist)",
+  SpaciousBalcony: "Großer, geräumiger Balkon, (Dach-)Terrasse, Loggia oder Winter-/Dachgarten (ab 4 m²)",
+  RollingShutters: "Rollläden",
+  CeilingDecoration: "Aufwändige Decken- und/oder Wandverkleidung (z.B. Stuck, Täfelung) in gutem Zustand in der überwiegenden Anzahl der Wohnräume",
+  AntiBurglary: "Zusätzliche Einbruchsicherung für die Wohnungstür bei verstärkten Türen",
 
-export default defineMessages({
-  WindowStyleSingle: {
-    id: "Apartment.WindowStyleSingleOfficial",
-    defaultMessage: "Überwiegend Einfachverglasung"
-  },
-  WindowStyleInsulating: {
-    id: "Apartment.WindowStyleInsulatingOfficial",
-    defaultMessage: "Überwiegend Wärmeschutzverglasung (Einbau ab 1995) oder Schallschutzfenster für Wohngebäude/Wohnungen, die vor 1995 bezugsfertig geworden sind"
-  },
-  HighGradeFlooring: {
-    id: "Apartment.HighGradeFlooringOfficial",
-    defaultMessage: "Hochwertiges Parkett, Natur-/Kunststein, Fliesen oder gleichwertiger Boden/-belag in der überwiegenden Zahl der Wohnräume"
-  },
-  InsufficientPower: {
-    id: "Apartment.InsufficientPowerOfficial",
-    defaultMessage: "Unzureichende Elektroinstallation, z.B. keine ausreichende Elektrosteigleitung und/oder VDE gerechte Elektroinstallation (z.B. kein FI-Schalter, Potentialausgleich)"
-  },
-  WallInstallationElectrical: {
-    id: "Apartment.WallInstallationElectricalOfficial",
-    defaultMessage: "Elektroinstallation überwiegend sichtbar auf Putz"
-  },
-  WallInstallationDrainage: {
-    id: "Apartment.WallInstallationDrainageOfficial",
-    defaultMessage: "Be- und Entwässerungsinstallation überwiegend auf Putz"
-  },
-  WallInstallationHeating: {
-    id: "Apartment.WallInstallationHeatingOfficial",
-    defaultMessage: "Heizungsrohre überwiegend nicht sichtbar"
-  },
-  ColdWaterMetering: {
-    id: "Apartment.ColdWaterMeteringOfficial",
-    defaultMessage: "Wohnungsbezogener Kaltwasserzähler in vor 1998 bezugsfertigen Gebäuden/Wohnungen, wenn der Mieter nicht die Kosten für Miete oder Leasing im Rahmen der Betriebskosten trägt"
-  },
-  BadFlatDesign: {
-    id: "Apartment.BadFlatDesignOfficial",
-    defaultMessage: "Schlechter Schnitt (z.B. gefangener Raum und/oder Durchgangsraum)"
-  },
-  StorageCabinet: {
-    id: "Apartment.StorageCabinetOfficial",
-    defaultMessage: "Einbauschrank oder Abstellraum innerhalb der Wohnung"
-  },
-  NoSpaceForWashing: {
-    id: "Apartment.NoSpaceForWashingOfficial",
-    defaultMessage: "Waschmaschine weder in Bad noch Küche stellbar oder nicht anschließbar"
-  },
-  LargeLiving: {
-    id: "Apartment.LargeLivingOfficial",
-    defaultMessage: "Großer, geräumiger Balkon, (Dach-)Terrasse, Loggia oder Winter-/Dachgarten (ab 4 m²)"
-  },
-  AccessibleDesign: {
-    id: "Apartment.AccessibleDesignOfficial",
-    defaultMessage: "Barrierearme Wohnungsgestaltung (Schwellenfreiheit in der Wohnung, schwellenarmer Übergang zu Balkon/Terrasse, ausreichende Bewegungsfreiheit in der Wohnung und/oder barrierearme Badgestaltung)"
-  },
-  NoBalcony: {
-    id: "Apartment.NoBalconyOfficial",
-    defaultMessage: "Kein Balkon, (Dach-)Terrasse, Loggia und Winter-/Dachgarten (gilt nicht, wenn das Merkmal aus baulichen und/oder rechtlichen Gründen nicht möglich oder nicht zulässig ist)"
-  },
-  SpaciousBalcony: {
-    id: "Apartment.SpaciousBalconyOfficial",
-    defaultMessage: "Großer, geräumiger Balkon, (Dach-)Terrasse, Loggia oder Winter-/Dachgarten (ab 4 m²)"
-  },
-  RollingShutters: {
-    id: "Apartment.RollingShuttersOfficial",
-    defaultMessage: "Rollläden"
-  },
-  CeilingDecoration: {
-    id: "Apartment.CeilingDecorationOfficial",
-    defaultMessage: "Aufwändige Decken- und/oder Wandverkleidung (z.B. Stuck, Täfelung) in gutem Zustand in der überwiegenden Anzahl der Wohnräume"
-  },
-  AntiBurglary: {
-    id: "Apartment.AntiBurglaryOfficial",
-    defaultMessage: "Zusätzliche Einbruchsicherung für die Wohnungstür bei verstärkten Türen"
-  },
+  TinySink: "Kein Handwaschbecken in Bad oder WC oder im Bad nur ein kleines Handwaschbecken",
+  ToiletNoVentilation: "WC ohne Lüftungsmöglichkeit oder Entlüftung",
+  BoardFlooring: "Dielenfußboden im Bad",
+  WarmWater: "Keine ausreichende Warmwasserversorgung",
+  FixedBathtub: "Einbauwanne",
+  InsufficientTiling: "Wände nicht ausreichend im Spritzwasserbereich von Waschbecken, Badewanne und/oder Dusche gefliest",
+  NoWindows: "Bad mit WC ohne Fenster",
+  NoShower: "Keine Duschmöglichkeit",
+  TinyBath: "Kleines Bad",
+  LargeSink: "Sehr großes Waschbecken (Außenmaß mindestens 80 cm breit) oder Doppelhandwaschbecken oder zwei getrennte Waschbecken",
+  HighClassFeatures: "Besondere und hochwertige Ausstattung",
+  ElectronicVentilation: "Innen liegendes Badezimmer mit moderner, gesteuerter Entlüftung ",
+  AdditionalWC: "Zweites WC in der Wohnung oder Bad und WC getrennt",
+  LargeBath: "Mindestens ein Bad größer als 8 m²",
+  FloorHeating: "Fußbodenheizung",
+  HighClassTiling: "Wandbekleidung und Bodenbelag hochwertig",
+  WallMountedWC: "Wandhängendes WC mit in der Wand eingelassenem Spülkasten",
+  TowelHeating: "Strukturheizkörper als Handtuchwärmer",
+  AdditionalShower: "Von der Badewanne getrennte zusätzliche Duschtasse oder -kabine",
 
+  EntranceCondition: "Treppenhaus/Eingangsbereich überwiegend in schlechtem Zustand",
+  DoorLock: "Hauseingangstür nicht abschließbar",
+  RepresentativeEntrance: "Repräsentativer/s oder hochwertig sanierter/s Eingangsbereich/Treppenhaus",
+  WellMaintained: "Überdurchschnittlich guter Instandhaltungszustand des Gebäude (-teil-)s, in dem sich die Wohnung befindet",
+  NoStorageRoom: "Kein Mieterkeller oder Kellerersatzraum zur alleinigen Nutzung des Mieters vorhanden",
+  BicycleRoom: "Abschließbarer leicht zugänglicher Fahrradabstellraum innerhalb des Gebäudes oder Fahrradabstellplätze mit Anschließmöglichkeit außerhalb des Gebäudes auf dem Grundstück",
+  CommunalSpace: "Zusätzliche und in angemessenem Umfang nutzbare Räume außerhalb der Wohnung in fußläufiger Entfernung",
+  LowMaintenance: "Schlechter Instandhaltungszustand ",
+  SideWing: "Lage im Seitenflügel oder Quergebäude bei verdichteter Bebauung",
+  NoLift: "Wohnung ab fünftem Obergeschoss ohne Personenaufzug",
+  Lift: "Personenaufzug bei weniger als fünf Obergeschossen",
+  NoIntercom: "Keine Gegen-/Wechselsprechanlage mit elektrischem Türöffner",
+  IntercomVideo: "Gegen-/Wechselsprechanlage mit Videokontakt und elektrischem Türöffner",
+  ModernizedHeatingSystem: "Wärmedämmung zusätzlich zur vorhandenen Bausubstanz oder Einbau/Installation einer modernen Heizanlage ab 01.01.2003 ",
+  InsufficientInsulation: "Unzureichende Wärmedämmung oder Heizanlage mit ungünstigem Wirkungsgrad",
+  Sub80: "Energieverbrauchskennwert < 80 kWh/(m²a)",
+  Sub100: "Energieverbrauchskennwert < 100 kWh/(m²a)",
+  Sub120: "Energieverbrauchskennwert < 120 kWh/(m²a)",
+  Super155: "Energieverbrauchskennwert > 155 kWh/(m²a)",
+  Super195: "Energieverbrauchskennwert > 195 kWh/(m²a)",
+  Super235: "Energieverbrauchskennwert > 235 kWh/(m²a)",
 
-  TinySink: {
-    id: "Bath.TinySinkOfficial",
-    defaultMessage: "Kein Handwaschbecken in Bad oder WC oder im Bad nur ein kleines Handwaschbecken"
-  },
-  ToiletNoVentilation: {
-    id: "Bath.ToiletNoVentilationOfficial",
-    defaultMessage: "WC ohne Lüftungsmöglichkeit oder Entlüftung"
-  },
-  BoardFlooring: {
-    id: "Bath.BoardFlooringOfficial",
-    defaultMessage: "Dielenfußboden im Bad"
-  },
-  WarmWater: {
-    id: "Bath.WarmWaterOfficial",
-    defaultMessage: "Keine ausreichende Warmwasserversorgung"
-  },
-  FixedBathtub: {
-    id: "Bath.FixedBathtubOfficial",
-    defaultMessage: "Einbauwanne"
-  },
-  InsufficientTiling: {
-    id: "Bath.InsufficientTilingOfficial",
-    defaultMessage: "Wände nicht ausreichend im Spritzwasserbereich von Waschbecken, Badewanne und/oder Dusche gefliest"
-  },
-  NoWindows: {
-    id: "Bath.NoWindowsOfficial",
-    defaultMessage: "Bad mit WC ohne Fenster"
-  },
-  NoShower: {
-    id: "Bath.NoShowerOfficial",
-    defaultMessage: "Keine Duschmöglichkeit"
-  },
-  TinyBath: {
-    id: "Bath.TinyBathOfficial",
-    defaultMessage: "Kleines Bad"
-  },
-  LargeSink: {
-    id: "Bath.LargeSinkOfficial",
-    defaultMessage: "Sehr großes Waschbecken (Außenmaß mindestens 80 cm breit) oder Doppelhandwaschbecken oder zwei getrennte Waschbecken"
-  },
-  HighClassFeatures: {
-    id: "Bath.HighClassFeaturesOfficial",
-    defaultMessage: "Besondere und hochwertige Ausstattung"
-  },
-  ElectronicVentilation: {
-    id: "Bath.ElectronicVentilationOfficial",
-    defaultMessage: "Innen liegendes Badezimmer mit moderner, gesteuerter Entlüftung "
-  },
-  AdditionalWC: {
-    id: "Bath.AdditionalWCOfficial",
-    defaultMessage: "Zweites WC in der Wohnung oder Bad und WC getrennt"
-  },
-  LargeBath: {
-    id: "Bath.LargeBathOfficial",
-    defaultMessage: "Mindestens ein Bad größer als 8 m²"
-  },
-  FloorHeating: {
-    id: "Bath.FloorHeatingOfficial",
-    defaultMessage: "Fußbodenheizung"
-  },
-  HighClassTiling: {
-    id: "Bath.HighClassTilingOfficial",
-    defaultMessage: "Wandbekleidung und Bodenbelag hochwertig"
-  },
-  WallMountedWC: {
-    id: "Bath.WallMountedWCOfficial",
-    defaultMessage: "Wandhängendes WC mit in der Wand eingelassenem Spülkasten"
-  },
-  TowelHeating: {
-    id: "Bath.TowelHeatingOfficial",
-    defaultMessage: "Strukturheizkörper als Handtuchwärmer"
-  },
-  AdditionalShower: {
-    id: "Bath.AdditionalShowerOfficial",
-    defaultMessage: "Von der Badewanne getrennte zusätzliche Duschtasse oder -kabine"
-  },
+  QuietStreet: "Besonders ruhige Lage",
+  Noisy: "Besonders lärmbelastete Lage",
+  Smelly: "Besonders geruchsbelastete Lage",
+  PrimeDowntown: "Bevorzugte Citylage",
+  NeglectedArea: "Lage in stark vernachlässigter Umgebung in einfacher Wohnlage",
+  NeatoBackyard: "Aufwändig gestaltetes Wohnumfeld auf dem Grundstück ",
+  PrivateBackyard: "Garten zur alleinigen Nutzung/Mietergarten ohne Entgelt oder zur Wohnung gehörender Garten mit direktem Zugang",
+  BicycleParking: "Keine Fahrradabstellmöglichkeit auf dem Grundstück",
+  Parking: "Vom Vermieter zur Verfügung gestelltes PKW-Parkplatzangebot in der Nähe",
 
-
-
-  EntranceCondition: {
-    id: "Building.EntranceConditionOfficial",
-    defaultMessage: "Treppenhaus/Eingangsbereich überwiegend in schlechtem Zustand"
-  },
-  DoorLock: {
-    id: "Building.DoorLockOfficial",
-    defaultMessage: "Hauseingangstür nicht abschließbar"
-  },
-  RepresentativeEntrance: {
-    id: "Building.RepresentativeEntranceOfficial",
-    defaultMessage: "Repräsentativer/s oder hochwertig sanierter/s Eingangsbereich/Treppenhaus"
-  },
-  WellMaintained: {
-    id: "Building.WellMaintainedOfficial",
-    defaultMessage: "Überdurchschnittlich guter Instandhaltungszustand des Gebäude (-teil-)s, in dem sich die Wohnung befindet"
-  },
-  NoStorageRoom: {
-    id: "Building.NoStorageRoomOfficial",
-    defaultMessage: "Kein Mieterkeller oder Kellerersatzraum zur alleinigen Nutzung des Mieters vorhanden"
-  },
-  BicycleRoom: {
-    id: "Building.BicycleRoomOfficial",
-    defaultMessage: "Abschließbarer leicht zugänglicher Fahrradabstellraum innerhalb des Gebäudes oder Fahrradabstellplätze mit Anschließmöglichkeit außerhalb des Gebäudes auf dem Grundstück"
-  },
-  CommunalSpace: {
-    id: "Building.CommunalSpaceOfficial",
-    defaultMessage: "Zusätzliche und in angemessenem Umfang nutzbare Räume außerhalb der Wohnung in fußläufiger Entfernung"
-  },
-  LowMaintenance: {
-    id: "Building.LowMaintenanceOfficial",
-    defaultMessage: "Schlechter Instandhaltungszustand "
-  },
-  SideWing: {
-    id: "Building.SideWingOfficial",
-    defaultMessage: "Lage im Seitenflügel oder Quergebäude bei verdichteter Bebauung"
-  },
-  NoLift: {
-    id: "Building.NoLiftOfficial",
-    defaultMessage: "Wohnung ab fünftem Obergeschoss ohne Personenaufzug"
-  },
-  Lift: {
-    id: "Building.LiftOfficial",
-    defaultMessage: "Personenaufzug bei weniger als fünf Obergeschossen"
-  },
-  NoIntercom: {
-    id: "Building.NoIntercomOfficial",
-    defaultMessage: "Keine Gegen-/Wechselsprechanlage mit elektrischem Türöffner"
-  },
-  IntercomVideo: {
-    id: "Building.IntercomVideoOfficial",
-    defaultMessage: "Gegen-/Wechselsprechanlage mit Videokontakt und elektrischem Türöffner"
-  },
-  ModernizedHeatingSystem: {
-    id: "Building.ModernizedHeatingSystemOfficial",
-    defaultMessage: "Wärmedämmung zusätzlich zur vorhandenen Bausubstanz oder Einbau/Installation einer modernen Heizanlage ab 01.01.2003 "
-  },
-  InsufficientInsulation: {
-    id: "Building.InsufficientInsulationOfficial",
-    defaultMessage: "Unzureichende Wärmedämmung oder Heizanlage mit ungünstigem Wirkungsgrad"
-  },
-  Sub80: {
-    id: "Building.Sub80Official",
-    defaultMessage: "Energieverbrauchskennwert < 80 kWh/(m²a)"
-  },
-  Sub100: {
-    id: "Building.Sub100Official",
-    defaultMessage: "Energieverbrauchskennwert < 100 kWh/(m²a)"
-  },
-  Sub120: {
-    id: "Building.Sub120Official",
-    defaultMessage: "Energieverbrauchskennwert < 120 kWh/(m²a)"
-  },
-  Super155: {
-    id: "Building.Super155Official",
-    defaultMessage: "Energieverbrauchskennwert > 155 kWh/(m²a)"
-  },
-  Super195: {
-    id: "Building.Super195Official",
-    defaultMessage: "Energieverbrauchskennwert > 195 kWh/(m²a)"
-  },
-  Super235: {
-    id: "Building.Super235Official",
-    defaultMessage: "Energieverbrauchskennwert > 235 kWh/(m²a)"
-  },
-
-
-
-  QuietStreet: {
-    id: "Environment.QuietStreetOfficial",
-    defaultMessage: "Besonders ruhige Lage"
-  },
-  Noisy: {
-    id: "Environment.NoisyOfficial",
-    defaultMessage: "Besonders lärmbelastete Lage"
-  },
-  Smelly: {
-    id: "Environment.SmellyOfficial",
-    defaultMessage: "Besonders geruchsbelastete Lage"
-  },
-  PrimeDowntown: {
-    id: "Environment.PrimeDowntownOfficial",
-    defaultMessage: "Bevorzugte Citylage"
-  },
-  NeglectedArea: {
-    id: "Environment.NeglectedAreaOfficial",
-    defaultMessage: "Lage in stark vernachlässigter Umgebung in einfacher Wohnlage"
-  },
-  NeatoBackyard: {
-    id: "Environment.NeatoBackyardOfficial",
-    defaultMessage: "Aufwändig gestaltetes Wohnumfeld auf dem Grundstück "
-  },
-  PrivateBackyard: {
-    id: "Environment.PrivateBackyardOfficial",
-    defaultMessage: "Garten zur alleinigen Nutzung/Mietergarten ohne Entgelt oder zur Wohnung gehörender Garten mit direktem Zugang"
-  },
-  BicycleParking: {
-    id: "Environment.BicycleParkingOfficial",
-    defaultMessage: "Keine Fahrradabstellmöglichkeit auf dem Grundstück"
-  },
-  Parking: {
-    id: "Environment.ParkingOfficial",
-    defaultMessage: "Vom Vermieter zur Verfügung gestelltes PKW-Parkplatzangebot in der Nähe"
-  },
-
-
-
-  NoVentilation: {
-    id: "FeatureOfficialNames.NoVentilationOfficial",
-    defaultMessage: "Küche ohne Fenster und ohne ausreichende Entlüftung"
-  },
-  ExtractorHood: {
-    id: "FeatureOfficialNames.ExtractorHoodOfficial",
-    defaultMessage: "Dunstabzug"
-  },
-  NoHeating: {
-    id: "FeatureOfficialNames.NoHeatingOfficial",
-    defaultMessage: "Küche nicht beheizbar oder Holz-/Kohleheizung"
-  },
-  HighGradeFloor: {
-    id: "FeatureOfficialNames.HighGradeFloorOfficial",
-    defaultMessage: "Hochwertige Fliesen, hochwertiges Linoleum, hochwertiges Feuchtraumlaminat, Parkett, Terrazzo als Bodenbelag jeweils in gutem Zustand"
-  },
-  FittedKitchen: {
-    id: "FeatureOfficialNames.FittedKitchenOfficial",
-    defaultMessage: "Einbauküche mit Ober- und Unterschränken sowie Herd und Spüle"
-  },
-  KitchenLiving: {
-    id: "FeatureOfficialNames.KitchenLivingOfficial",
-    defaultMessage: "Separate Küche mit mind. 14 m² Grundfläche"
-  },
-  NoStove: {
-    id: "FeatureOfficialNames.NoStoveOfficial",
-    defaultMessage: "Keine Kochmöglichkeit oder Gas-/Elektroherd ohne Backofen"
-  },
-  HighGradeStove: {
-    id: "FeatureOfficialNames.HighGradeStoveOfficial",
-    defaultMessage: "Ceran-Kochfeld oder Induktions-Kochfeld"
-  },
-  NoSink: {
-    id: "FeatureOfficialNames.NoSinkOfficial",
-    defaultMessage: "Keine Spüle"
-  },
-  NoDishwasherSpace: {
-    id: "FeatureOfficialNames.NoDishwasherSpaceOfficial",
-    defaultMessage: "Geschirrspüler in der Küche nicht stellbar oder anschließbar"
-  },
-  FreezerProvided: {
-    id: "FeatureOfficialNames.FreezerProvidedOfficial",
-    defaultMessage: "Kühlschrank"
-  },
-  InsufficientWarmWater: {
-    id: "FeatureOfficialNames.InsufficientWarmWaterOfficial",
-    defaultMessage: "Keine ausreichende Warmwasserversorgung"
-  }
-});
+  NoVentilation: "Küche ohne Fenster und ohne ausreichende Entlüftung",
+  ExtractorHood: "Dunstabzug",
+  NoHeating: "Küche nicht beheizbar oder Holz-/Kohleheizung",
+  HighGradeFloor: "Hochwertige Fliesen, hochwertiges Linoleum, hochwertiges Feuchtraumlaminat, Parkett, Terrazzo als Bodenbelag jeweils in gutem Zustand",
+  FittedKitchen: "Einbauküche mit Ober- und Unterschränken sowie Herd und Spüle",
+  KitchenLiving: "Separate Küche mit mind. 14 m² Grundfläche",
+  NoStove: "Keine Kochmöglichkeit oder Gas-/Elektroherd ohne Backofen",
+  HighGradeStove: "Ceran-Kochfeld oder Induktions-Kochfeld",
+  NoSink: "Keine Spüle",
+  NoDishwasherSpace: "Geschirrspüler in der Küche nicht stellbar oder anschließbar",
+  FreezerProvided: "Kühlschrank",
+  InsufficientWarmWater: "Keine ausreichende Warmwasserversorgung"
+};
